93bb4d8f4deb7021eeb49a7e75e6823bfebe7048
Nicholas Giudice <Nicholas Giudice@DESKTOP-SAHCSQN>
2017-02-25 19:20:55 -0300

    This project recognizes when you raise the right hand.

---
+++ Kinect/AuxiliarKinect/FuncoesBasicas/InicializadorKinect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace AuxiliarKinect.FuncoesBasicas
+{
+    public static class InicializadorKinect
+    {
+        public static KinectSensor InicializarPrimeiroSensor(int anguloElevacaoInicial)
+        {
+            KinectSensor kinect = KinectSensor.KinectSensors.FirstOrDefault(k => k.Status == KinectStatus.Connected);
+
+            if (kinect == null)
+            {
+                throw new Exception("Kinect não encontrado!");       
+            }
+
+            kinect.Start();
+            kinect.ElevationAngle = anguloElevacaoInicial;
+
+            return kinect;
+        }
+
+    }
+}
---
+++ Kinect/Kinect/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using AuxiliarKinect.FuncoesBasicas;
+using Microsoft.Kinect;
+using System.Linq;
+
+namespace Kinect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        bool MaoDireitaAcimaCabeca; 
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InicializarSensor();
+
+
+        }
+
+        private void InicializarSensor()
+        {
+            KinectSensor kinect = InicializadorKinect.InicializarPrimeiroSensor(0);
+            kinect.SkeletonStream.Enable();
+
+            kinect.SkeletonFrameReady += KinectEvent;
+        }
+
+
+        private void ExecutarRegraMaoDireitaAcimaDaCabeca(SkeletonFrame quadroAtual)
+        {
+            Skeleton[] esqueletos = new Skeleton[6]; quadroAtual.CopySkeletonDataTo(esqueletos);
+            Skeleton usuario = esqueletos.FirstOrDefault(esqueleto => esqueleto.TrackingState == SkeletonTrackingState.Tracked);
+            if (usuario != null)
+            {
+                Joint maoDireita = usuario.Joints[JointType.HandRight]; Joint cabeca = usuario.Joints[JointType.Head]; bool novoTesteMaoDireitaAcimaCabeca = maoDireita.Position.Y > cabeca.Position.Y;
+                if (MaoDireitaAcimaCabeca != novoTesteMaoDireitaAcimaCabeca)
+                {
+                    MaoDireitaAcimaCabeca = novoTesteMaoDireitaAcimaCabeca;
+                    if (MaoDireitaAcimaCabeca) MessageBox.Show("A mão direita está acima da cabeça!");
+                }
+            }
+        }
+
+        private void KinectEvent(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame quadroAtual = e.OpenSkeletonFrame())
+            {
+                if (quadroAtual != null)
+                {
+                    ExecutarRegraMaoDireitaAcimaDaCabeca(quadroAtual);
+                }
+            }
+        }
+
+
+    }
+}
